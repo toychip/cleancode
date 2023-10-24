8a85359b0a4ed17af4798c44fcf636b3a7d09bb8
toychip <dlawnsgud427@naver.com>
2023-10-24 13:30:07 +0900

    Section2: JAVA 8 API interface default, static method, 정렬 활용

--- java8to11/src/main/java/section1/functioninterface/Main.java
+++ java8to11/src/main/java/section1/functioninterface/Main.java
@@ -1,6 +1,7 @@
 package section1.functioninterface;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Spliterator;
 
@@ -13,31 +14,21 @@ public class Main {
         name.add("toychip");
         name.add("junjun");
         name.add("ofo");
+        name.add("abcdefg");
+        name.add("가"); // 한글은 가장 순서가 늦음
 
-        // 아래 둘은 같은 메서드다. 순회하면서 모두 print한다.
-//        name.forEach(s -> System.out.println(s));
+        // "t"로 시작하는 것 제외
+        name.removeIf(s -> s.startsWith("t"));
 //        name.forEach(System.out::println);
 
-        // spliterator 쪼개진 것들의 String 들이 존재할 때까지 반복해서 Print
-        Spliterator<String> spliterator1 = name.spliterator();
-        System.out.println("-----  flag 1  ----");
-        while (spliterator1.tryAdvance(System.out::println));
+        // 문자열 정렬
+        name.sort(String::compareToIgnoreCase);
 
-        System.out.println("-----------------자르기 전--------------------");
-        // 아래의 trySplit을 사용하면 반으로 쪼갬. 4개면 2, 2. 3개면 1,2 이런 방식
-        // name 리스트를 string을 개당 나눠서 spliterator1 넣음.
-        spliterator1 = name.spliterator();
+        // 기존 정렬의 역순으로 정렬
+        Comparator<String> compareToIgnoreCase = String::compareToIgnoreCase;
+        name.sort(compareToIgnoreCase.reversed());
 
-        // spliterator1에 있는 값을 반으로 잘라서, 2엔다가 넣음.
-        Spliterator<String> spliterator2 = spliterator1.trySplit();
-
-        // spliterator1을 print
-        while (spliterator1.tryAdvance(System.out::println));
-        System.out.println("================");
-        // spliterator2를 print
-        while (spliterator2.tryAdvance(System.out::println));
-
-
+        name.forEach(System.out::println);
     }
 }
 
