1b2e119a3f1f03cdbe52f0cc461940c8ce78feb8
toychip <dlawnsgud427@naver.com>
2023-10-24 09:15:00 +0900

    Section1: 함수형 인터페이스와 람다 - 자바 제공 함수형 인터페이스 Predicate<T>

--- java8to11/src/main/java/section1/functioninterface/Main.java
+++ java8to11/src/main/java/section1/functioninterface/Main.java
@@ -1,18 +1,18 @@
 package section1.functioninterface;
 
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Supplier;
+import java.util.function.Predicate;
 
 public class Main {
 
     // 생성한 클래스를 실행하는 메인 메서드
     public static void main(String[] args) {
 
-        // 무조건 10을 return하는 함수
-        // T타입의 값을 제공하는 함수 인터페이스
-        Supplier<Integer> get10 = () -> 10;
-        Integer integer = get10.get();
-        System.out.println("integer = " + integer); // integer = 10
+        // 어떠한 인자 값을 받아서 True or False를 return하는 함수
+        Predicate<String> starts = (s) -> s.startsWith("Jun");
+        boolean junhyoung = starts.test("Junhyoung");
+        boolean kyoung = starts.test("kyoung");
+
+        System.out.println("junhyoung = " + junhyoung);
+        System.out.println("kyoung = " + kyoung);
     }
 }
