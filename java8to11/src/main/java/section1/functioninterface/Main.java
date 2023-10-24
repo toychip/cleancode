efc9553844f8f82c0ea2d1495ab5136d1e4d6e8f
toychip <dlawnsgud427@naver.com>
2023-10-24 09:18:48 +0900

    Section1: 함수형 인터페이스와 람다 - 자바 제공 함수형 인터페이스 Predicate<T> 2

--- java8to11/src/main/java/section1/functioninterface/Main.java
+++ java8to11/src/main/java/section1/functioninterface/Main.java
@@ -8,11 +8,15 @@ public class Main {
     public static void main(String[] args) {
 
         // 어떠한 인자 값을 받아서 True or False를 return하는 함수
-        Predicate<String> starts = (s) -> s.startsWith("Jun");
-        boolean junhyoung = starts.test("Junhyoung");
-        boolean kyoung = starts.test("kyoung");
+        // integer로 받아서 홀수인지 판별하는 함수, 마찬가지로 Integer도 받을 수 잇음
+        Predicate<Integer> isOdd = (i) -> i % 2 != 0;
 
-        System.out.println("junhyoung = " + junhyoung);
-        System.out.println("kyoung = " + kyoung);
+        boolean test = isOdd.test(2);
+        boolean test2 = isOdd.test(3);
+
+        System.out.println("test = " + test);
+        System.out.println("test2 = " + test2);
+
+        // 또한 returnr값이 boolean이기 때문에 or, and 등 조합이 가능하다.
     }
 }
