c4ef9427e8ef82cdafa836e68188cedddc4e9284
toychip <dlawnsgud427@naver.com>
2023-10-24 09:29:43 +0900

    Section1: 람다 표현식 - 가장 기본적인 사용법

--- java8to11/src/main/java/section1/functioninterface/Main.java
+++ java8to11/src/main/java/section1/functioninterface/Main.java
@@ -3,6 +3,7 @@ package section1.functioninterface;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
 
 public class Main {
@@ -10,13 +11,13 @@ public class Main {
     // 생성한 클래스를 실행하는 메인 메서드
     public static void main(String[] args) {
 
-        Function<Integer, Integer> plus10 = (number) -> number + 10;
+        // 람다 표현식
+        // ()가 인자를 나타냄, 아래는 인자가 없는 Supplier 인터페이스 사용
+        Supplier<Integer> supplier = () -> 10;
 
-        // 입력하는 값과, return하는 값이 같다면 아래처럼 UnaryOperator를 사용할 수 있다.
-        UnaryOperator<Integer> plus10_2 = (number) -> number + 10;
+        // 인자가 2개인 경우 아래와 같이 사용. 인자 2개인 BinaryOperator 인터페이스 사용
+        BinaryOperator<Integer> binaryOperator = (a, b) -> a + b + 1;
 
-        // 입력하는 2개의 매개변수와 return이 모두 같을 때 아래와 같이 사용 가능
-        BinaryOperator<Integer> plus10_3 = (number1, number2) -> number1 + number2 + 3;
 
 
     }
