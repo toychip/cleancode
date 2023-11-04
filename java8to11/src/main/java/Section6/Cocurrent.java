8fcca80735324638453fe52f141a77eb0a09f3f5
toychip <dlawnsgud427@naver.com>
2023-11-04 23:45:21 +0900

    Section6: Thread를 만드는 첫번째 방법

---
+++ java8to11/src/main/java/Section6/Cocurrent.java
@@ -0,0 +1,31 @@
+package Section6;
+
+public class Cocurrent {
+    public static void main(String[] args) {
+
+//        System.out.println("나는 main Thread");
+//        System.out.println(Thread.currentThread().getName());
+
+        /*
+        code 상으로는 THread: 가 먼저 실행되고 hello가 실행될 줄 알았느데 아래처럼 나옴
+         */
+
+        MyThread myThread = new MyThread();
+        myThread.start();
+
+        System.out.println("Hello");
+    }
+
+    /*
+    main Thread에서 다른 Thread를 만들 수 있음
+    크게 2가지가 있음 아래는 불편한 방법
+     */
+
+    static class MyThread extends Thread{
+        @Override
+        public void run() {
+            System.out.println("Thread: " + Thread.currentThread().getName());
+        }
+    }
+
+}
